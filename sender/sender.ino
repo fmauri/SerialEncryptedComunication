80f43c02b2b874e3b1f132176b892000199f57f9
fmauri <mauri95francesco@gmail.com>
2018-04-06 19:25:02 +0200

    sending working with fixed key

--- sender/sender.ino
+++ sender/sender.ino
@@ -5,10 +5,10 @@
 
 char key[5] = "Mordo"; //String data
 char msg[5] = "CoTam";
-int address = 0x8a75;
+long address = 35445; // 0x8a75;
 char cipher [5] = "somet";
 char negation;
-int conc = 0;
+//int conc = 0;
 IRsend irsend;
 
 void setup() {
@@ -19,13 +19,14 @@ void setup() {
 void loop() {
   Serial.write(key,5); //Write the serial data
   for(int i=0; i <5;i++){
+    long conc = 0;
     cipher[i] = key[i] ^ msg[i];
     negation = ~cipher[i];
-    conc += ((cipher[i]<<8)+negation);
-    conc += (address<<16);
+    conc = ((((cipher[i]+1)<<8))+negation);
+    conc =(conc)+ (address<<16);
     irsend.sendNEC(conc, 32);
-    delay(100);
+    delay(1000);
   } 
-  irsend.sendNEC(255, 32); 
+//  irsend.sendNEC(255, 32); 
   delay(5000);
 }
