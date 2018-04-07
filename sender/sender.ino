377e531b311956ef240a33a8d9b50943c993bc52
fmauri <mauri95francesco@gmail.com>
2018-04-07 10:21:09 +0200

    super pro sender with fixed key

--- sender/sender.ino
+++ sender/sender.ino
@@ -8,7 +8,10 @@ char msg[5] = "CoTam";
 long address = 35445; // 0x8a75;
 char cipher [5] = "somet";
 char negation;
-//int conc = 0;
+long start = 0x8a75FFFF;
+long ending = 0x8a750000;
+long conc = 0;
+
 IRsend irsend;
 
 void setup() {
@@ -18,15 +21,23 @@ void setup() {
 
 void loop() {
   Serial.write(key,5); //Write the serial data
+//  irsend.sendNEC(start, 32);
+  delay(1000);
   for(int i=0; i <5;i++){
-    long conc = 0;
     cipher[i] = key[i] ^ msg[i];
+//    Serial.print(i);
+//    Serial.print(" ");  
+//    Serial.println(cipher[i],HEX);
     negation = ~cipher[i];
     conc = ((((cipher[i]+1)<<8))+negation);
     conc =(conc)+ (address<<16);
     irsend.sendNEC(conc, 32);
+//    Serial.print(i);
+//    Serial.print(" ");
+//    Serial.println(conc,HEX);
     delay(1000);
-  } 
-//  irsend.sendNEC(255, 32); 
+  }
+  irsend.sendNEC(start, 32);
   delay(5000);
 }
+
