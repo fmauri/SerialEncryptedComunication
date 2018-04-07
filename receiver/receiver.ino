2f7a963d176e0cc4c24e38f503ebd98579df91c6
fmauri <mauri95francesco@gmail.com>
2018-04-07 10:31:08 +0200

    super pro receiver

--- receiver/receiver.ino
+++ receiver/receiver.ino
@@ -1,13 +1,52 @@
-char mystr[5]; //Initialized variable to store recieved data
+#include <IRremote.h>
 
-void setup() {
-  // Begin the Serial at 9600 Baud
+int RECV_PIN = 11;
+
+IRrecv irrecv(RECV_PIN);
+
+decode_results results;
+int address_rev = 0x8a75;
+char prevCmd = ' ';
+void setup()
+{
   Serial.begin(9600);
+  irrecv.enableIRIn(); // Start the receiver
+}
+int iteration = 0;
+char msg[5]; // Received via IR (wireless)
+char key[5]; // Received via UART (wires)
+void loop()
+{
+  Serial.readBytes(key,5); //Receive KEY from UART
+  if (irrecv.decode(&results)) 
+  {
+    int i = 0;
+    for(i=0; i<16; i++)
+      if(bitRead(results.value, i)==bitRead(address_rev, i)) 
+      {
+        char addr = results.value >> 24;
+        char revAddr = (( (results.value)  << 8 ) >> 24);
+        bool areTheSame = (addr == ~revAddr);
+        if(areTheSame)
+        {
+          char cmd = results.value << 16 >> 24;
+          char revCmd = results.value << 24 >> 24;
+          if(cmd == ~revCmd && prevCmd != cmd) {
+            prevCmd = cmd;
+            msg[iteration] = cmd ^ key[iteration];
+            Serial.print(msg[iteration]);
+            iteration = (iteration + 1); 
+          } else if (cmd == revCmd) {
+            iteration = 0;
+            Serial.println("");
+          } else { }
+        } else {
+          Serial.println("SOMETHING VERY STRANGE");
+        }
+      }
+         
+    irrecv.resume(); // Receive the next value }
+  
+  }
 }
 
-void loop() {
-  Serial.readBytes(mystr,5); //Read the serial data and store in var
-  Serial.print("\n");
-  Serial.print(mystr);
-  delay(1000);
-}
