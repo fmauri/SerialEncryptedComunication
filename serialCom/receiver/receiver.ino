a7990e2fb94f43e536ca8896dd5736fb6abafba8
Arkadiusz Lewandowski <arkadiusz.lewandowski@identt.pl>
2018-04-07 13:48:11 +0200

    receiver with msg decrypted from a5/1

--- serialCom/receiver/receiver.ino
+++ serialCom/receiver/receiver.ino
@@ -1,4 +1,3 @@
-char mystr[5]; //Initialized variable to store recieved data
 #include <IRremote.h>
 
 int RECV_PIN = 11;
@@ -14,52 +13,35 @@ void setup()
   irrecv.enableIRIn(); // Start the receiver
 }
 int iteration = 0;
-char msg[6] = "BEDZI";
+char msg[6]; // Received via IR (wireless)
+char key[6]; // Received via UART (wires)
 void loop()
 {
-  Serial.readBytes(mystr,5); //Read the serial data and store KEY
-//  Serial.print(mystr);
-  
+  Serial.readBytes(key,6); //Receive KEY from UART
+  Serial.println(key);
   if (irrecv.decode(&results)) 
   {
     int i = 0;
     for(i=0; i<16; i++)
       if(bitRead(results.value, i)==bitRead(address_rev, i)) 
       {
-//        Serial.println(results.value, BIN);
-        char addr = results.value >> 24;// << 24;
+        char addr = results.value >> 24;
         char revAddr = (( (results.value)  << 8 ) >> 24);
         bool areTheSame = (addr == ~revAddr);
-//        Serial.println(addr, BIN);
-//        Serial.println(revAddr, BIN);
-//        Serial.println(areTheSame);
         if(areTheSame)
         {
           char cmd = results.value << 16 >> 24;
           char revCmd = results.value << 24 >> 24;
           if(cmd == ~revCmd && prevCmd != cmd) {
             prevCmd = cmd;
-            msg[iteration] = cmd ^ mystr[iteration];
-            Serial.print(iteration);
-            Serial.print(" ");
-            Serial.print(cmd, HEX);
-            Serial.print(" ");
-            Serial.print(mystr[iteration]);
-            Serial.print(" ");
-            Serial.println(msg[iteration]);
-            iteration = (iteration + 1); 
+            msg[iteration] = cmd ^ key[iteration];
+            iteration = (iteration + 1);
           } else if (cmd == revCmd) {
-            iteration = 0;
-            Serial.println("-----");
-          } else {
-            Serial.println("+++");
-          }
-        } else {
-          Serial.println("SOMETHING VERY STRANGE");
-        }
+            Serial.println(msg);
+            iteration = 0; // Message will start again
+          } else { }
+        } else { }
       }
-         
     irrecv.resume(); // Receive the next value }
-  
   }
 }
